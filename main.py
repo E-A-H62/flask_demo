178aedd9d477627c7eaf09c4191d0cf96ac1e771
Elena Hernandez <eahernandez1212@gmail.com>
2024-07-03 21:46:02 +0000

    Editing code to fix form submission

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        flash(f'Account created for {form.username.data}!', 'success')
+        # flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('hello_world'))
     return render_template('register.html', title='Register', form=form)
 
